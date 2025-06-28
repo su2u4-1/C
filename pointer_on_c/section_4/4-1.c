b7380205dd7b407439d2bf263d73480288ca9ef7
su2u4 <Joey23.su@gmail.com>
2025-06-28 23:54:04 +0800

    revision 4-1.c

--- pointer_on_c/section_4/4-1.c
+++ pointer_on_c/section_4/4-1.c
@@ -1,18 +1,22 @@
+#include <math.h>
 #include <stdio.h>
 
 int main() {
-    double t = 1, p = 0;
-    int n;
+    float t = 1, p = 0, n;
     printf("Enter a positive integer: ");
-    scanf("%d", &n);
+    scanf("%f", &n);
     if (n < 0) {
         printf("Please enter a positive integer.\n");
         return 1;
+    } else if (n == 0) {
+        printf("The square root of 0 is 0.\n");
+        return 0;
     }
-    while (t != p) {
+    do {
         p = t;
         t = (t + n / t) / 2;
-    }
+        printf("t: %.15e, p: %.15e\n", t, p);
+    } while (t != p);
     printf("The square root of %d is approximately %f\n", n, t);
 
     return 0;
